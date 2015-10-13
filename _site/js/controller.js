68483409f0cac368b8aa0c3ffc0920a6ab5e821d
Vikash Loomba <loomba.vikash@gmail.com>
2015-10-13 15:35:48 -0700

    Created new skills section

--- _site/js/controller.js
+++ _site/js/controller.js
@@ -1,14 +1,64 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ngAnimate']).config(function ($interpolateProvider) {
+     $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+});
 
 app.controller('skillCtrl', function($scope) {
-    $scope.skills = [{type:'Mobile Development', skill:'Swift', icon: 'devicon-apple-original colored'},
-                         {type:'Front-end language', skill:'HTML5', icon: 'devicon-html5-plain-wordmark colored'},
-                         {type:'Front-end language', skill:'CSS3', icon: 'devicon-css3-plain-wordmark colored'},
-                         {type:'Front-end framework', skill:'AngularJS', icon: 'devicon-angularjs-plain colored'},
-                         {type:'Front-end framework', skill:'Bootstrap', icon: 'devicon-bootstrap-plain-wordmark colored'},
-                         {type:'Back-end language', skill:'JavaScript', icon: 'devicon-javascript-plain colored'},
-                         {type:'Back-end language', skill:'PHP', icon: 'devicon-php-plain colored'}, 
-                         {type:'Back-end framework', skill:'CodeIgniter', icon: 'devicon-codeigniter-plain colored'}, 
-                         {type:'Back-end framework', skill:'NodeJS', icon: 'devicon-nodejs-plain colored'},
-                         {type:'Back-end framework', skill:'Meteor', icon: 'devicon-meteor-plain colored'}];
+     $scope.skills = [{dev_type:'Mobile', type: 'language', skill:'Swift', icon: 'devicon-apple-original colored'},
+                         {dev_type:'Front-end', type: 'language', skill:'HTML5', icon: 'devicon-html5-plain-wordmark colored'},
+                         {dev_type:'Front-end', type: 'language', skill:'CSS3', icon: 'devicon-css3-plain-wordmark colored'},
+                         {dev_type:'Front-end', type: 'framework', skill:'AngularJS', icon: 'devicon-angularjs-plain-wordmark colored'},
+                         {dev_type:'Front-end', type: 'framework', skill:'Bootstrap', icon: 'devicon-bootstrap-plain-wordmark colored'},
+                         {dev_type:'Back-end', type: 'language', skill:'JavaScript', icon: 'devicon-javascript-plain colored'},
+                         {dev_type:'Back-end', type: 'language', skill:'PHP', icon: 'devicon-php-plain colored'}, 
+                         {dev_type:'Back-end', type: 'framework', skill:'CodeIgniter', icon: 'devicon-codeigniter-plain-wordmark colored'}, 
+                         {dev_type:'Back-end', type: 'framework', skill:'NodeJS', icon: 'devicon-nodejs-plain-wordmark colored'},
+                         {dev_type:'Back-end', type: 'framework', skill:'Meteor', icon: 'devicon-meteor-plain-wordmark colored'}];
+     $scope.sortBack = function() {
+          if ($scope.backend == true) {
+               $scope.backend = false;
+               console.log('We just reset the backend bool', $scope.backend);
+          } else if ($scope.backend == false) {
+               $scope.allskills = false;
+               $scope.frontend = false;
+               $scope.mobile = false;
+               $scope.backend = true;
+               console.log('set the backend filter to true', $scope.backend);
+          }
+     };
+     $scope.sortFront = function () {
+          if ($scope.frontend == true) {
+               $scope.frontend = false;
+               console.log('We just reset the frontend bool', $scope.frontend);
+          } else if ($scope.frontend == false) {
+               $scope.allskills = false;
+               $scope.mobile = false;
+               $scope.backend = false;
+               $scope.frontend = true;
+               console.log('set the frontend filter to true', $scope.frontend);
+          }
+     };
+     $scope.sortMobile = function () {
+          if ($scope.mobile == true) {
+               $scope.mobile = false;
+               console.log('We just reset the mobile bool', $scope.mobile);
+          } else if ($scope.mobile == false) {
+               $scope.allskills = false;
+               $scope.backend = false;
+               $scope.frontend = false;
+               $scope.mobile = true;
+               console.log('set the mobile filter to true', $scope.mobile);
+          }
+     };
+     $scope.sortAll = function () {
+          if ($scope.allskills == true) {
+               $scope.allskills = false;
+
+          } else if ($scope.allskills == false) {
+               $scope.backend = false;
+               $scope.frontend = false;
+               $scope.mobile = false;
+               $scope.allskills = true;
+          };
+     }
+
 });
